61bfaee04be659c0e94ad78cb4215ce7abecde69
Клименко Андрей <aklimenko@diasoft.ru>
2016-12-08 17:20:10 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+var port = process.env.PORT||1337;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function (req,res) {res.status(200).send("Hello world!");});
+
+app.listen(port, function(){
+ console.log("Listening on port "+port);
+});
+
+app.post("/hello", function (req,res, next){
+ var username = req.body.user_name;
+ var botPayload ={
+text: "Hello "+username+ ", have a nice day!"
+};
+if (username !== "slackbot") {
+	return res.status(200).json(botPayload);
+} else {
+	return res.status(200).end();
+}
+});
